6e584fe16a42dfbe4c2746ef8d045237a1ae146d
Jojo <jojokoshy@users.noreply.github.com>
2020-01-10 08:22:34 +0800

    Update metastore-start.sh

--- hive/files/metastore-start.sh
+++ hive/files/metastore-start.sh
@@ -4,7 +4,7 @@
 /etc/bootstrap.sh
 
 # Start the metastore
-hive --service metastore --hiveconf hive.root.logger=DEBUG,console --hiveconf fs.azure.account.key.${AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net=${AZURE_STORAGE_ACCOUNT_KEY} --hiveconf fs.abfs.impl=org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem --hiveconf fs.AbstractFileSystem.abfs.impl=org.apache.hadoop.fs.azurebfs.Abfs --hiveconf fs.azure.account.oauth.provider.type=org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider --hiveconf fs.azure.account.oauth2.client.id=${ADLS_CLIENT_ID} --hiveconf fs.azure.account.oauth2.client.secret=${ADLS_CLIENT_SECRET} --hiveconf fs.azure.account.oauth2.client.endpoint=https://login.microsoftonline.com/${ADLS_TENANT_ID}/oauth2/token
+hive --service metastore --hiveconf hive.root.logger=DEBUG,console --hiveconf fs.azure.account.key.${AZURE_STORAGE_ACCOUNT_NAME}.dfs.core.windows.net=${AZURE_STORAGE_ACCOUNT_KEY} --hiveconf fs.abfs.impl=org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem --hiveconf fs.AbstractFileSystem.abfs.impl=org.apache.hadoop.fs.azurebfs.Abfs --hiveconf fs.azure.account.oauth.provider.type=org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider --hiveconf fs.azure.account.oauth2.client.id=${ADLS_CLIENT_ID} --hiveconf fs.azure.account.oauth2.client.secret=${ADLS_CLIENT_SECRET} --hiveconf fs.azure.account.oauth2.client.endpoint=https://login.microsoftonline.com/${ADLS_TENANT_ID}/oauth2/token
 
 # Spin wait
 while true; do sleep 1000; done
